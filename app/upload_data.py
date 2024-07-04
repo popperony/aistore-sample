caca30009993e7b44548cee1f9d1f8a91393d6db
popperony <popperony@yandex.ru>
2024-07-05 00:17:27 +0300

    refactor upload

--- app/upload_data.py
+++ app/upload_data.py
@@ -20,28 +20,18 @@ def download_and_upload_data(client_url: str, bucket_name: str, prefix: str, hf_
     client = Client(client_url)
     bucket = client.bucket(bucket_name)
 
-    temp_dir = "data/ilsvrc_imagenet1k/"
-    os.makedirs(temp_dir, exist_ok=True)
-    print('Start upload dataset...')
-
-    for idx, sample in enumerate(dataset):
+    for id_, sample in enumerate(dataset):
         image = sample['image']
 
-        image_path = os.path.join(temp_dir, f"train_{idx}.jpg")
+        image_path = f"temp_train_{id_}.jpg"
         image.save(image_path)
 
-        try:
-            object_name = f"{prefix}/train/{idx}.jpg"
-            with open(image_path, 'rb') as f:
-                bucket.object(object_name).put(f.read())
+        object_name = f"{prefix}/train/{id_}.jpg"
+        with open(image_path, 'rb') as f:
+            print(f'Uploading {object_name}')
+            bucket.put_object(object_name, data=f, content_type='image/jpeg')
 
-            print(f"Uploaded {object_name}")
-        finally:
-            if os.path.exists(image_path):
-                os.remove(image_path)
-                print(f"Deleted temporary file {image_path}")
-            else:
-                print(f"File {image_path} does not exist")
+        os.remove(image_path)
 
 
 if __name__ == "__main__":
