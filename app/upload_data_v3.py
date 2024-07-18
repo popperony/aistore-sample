4b05712a9a249359c93a703d553def29871956b4
popperony <popperony@yandex.ru>
2024-07-18 19:48:45 +0300

    fix bucket

--- app/upload_data_v3.py
+++ app/upload_data_v3.py
@@ -1,12 +1,51 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision import models, transforms
-from aistore.pytorch import AISFileLister, AISFileLoader
-from torchdata.datapipes.iter import FileLister, FileLoader, TarArchiveReader
+from PIL import Image
+import tarfile
+from io import BytesIO
+from aistore.client import Client
 
 number_of_classes = 10
+
+bucket_name = "imagenet-data"
+tar_file_path ="train_images_0.tar.gz"
+ais_client = Client("http://localhost:51080")
+
+
+class AISTarDataset(Dataset):
+    def __init__(self, client, bucket_name, tar_path, transform=None):
+        self.client = client
+        self.bucket_name = bucket_name
+        self.tar_path = tar_path
+        self.transform = transform
+        self.members = []
+
+        # Download the tar file from AIStore
+        self.download_tar()
+
+        # Open the tar file and get the member list
+        with tarfile.open(fileobj=BytesIO(self.tar_content), mode='r:gz') as tar:
+            self.members = [m for m in tar.getmembers() if m.isfile() and m.name.endswith('.jpg')]
+
+    def download_tar(self):
+        obj = self.client.get_object(self.bucket_name, self.tar_path)
+        self.tar_content = obj.read_all()
+
+    def __len__(self):
+        return len(self.members)
+
+    def __getitem__(self, idx):
+        member = self.members[idx]
+        with tarfile.open(fileobj=BytesIO(self.tar_content), mode='r:gz') as tar:
+            img_file = tar.extractfile(member)
+            img = Image.open(BytesIO(img_file.read()))
+            if self.transform:
+                img = self.transform(img)
+        return img
+
 
 transform = transforms.Compose([
     transforms.Resize(256),
@@ -14,37 +53,30 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
-print('Загрузка данных из бакета')
 
-bucket_name = 'imagenet-data'
-lister = AISFileLister(bucket_name=bucket_name, extension_filter='.tar.gz')
-loader = AISFileLoader(bucket_name=bucket_name)
 
-files = lister.filelist()
-dataset = loader.load_files(files).map(TarArchiveReader).map(lambda x: (transform(x[0]), x[1])).shuffle().batch(32)
+dataset = AISTarDataset(ais_client, bucket_name, tar_file_path, transform=transform)
 
-print('Инициализация модели')
 model = models.resnet18(pretrained=True)
 model.fc = nn.Linear(model.fc.in_features, number_of_classes)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
+train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-def train_model(dataloader, model, criterion, optimizer, num_epochs=10):
+
+def train_model(dataloader, model, criterion, optimizer, num_epochs=3):
     model.train()
     for epoch in range(num_epochs):
         running_loss = 0.0
-        for inputs, labels in dataloader:
+        for inputs in dataloader:
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, torch.zeros(len(inputs), dtype=torch.long))
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
         print(f'Epoch {epoch+1}: Loss {running_loss/len(dataloader)}')
 
-print('Подготовка данных')
-train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-print('Тренировка модели')
 train_model(train_loader, model, criterion, optimizer)
